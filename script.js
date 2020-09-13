c6de87a2111154ec8269478840a4898224cd834c
Yui <ytkakentan@yahoo.co.jp>
2020-09-13 18:21:48 +0900

    first add

--- script.js
+++ script.js
@@ -10,10 +10,30 @@ function showScore(){
 2問目の正解はAのバイカル湖
 3問目の正解はCのレンブラント
 **************/
-
+const button = document.querySelector('button');
 function answerQuiz1(){
 //問題1の処理をここに記述
+    const quiz_1 = document.getElementById('quiz-1');
+    const rightAnswer = quiz_1.answer.value
+    switch(rightAnswer){
+        case 'a':
+            console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
+            console.log('正解です');
+            console.log('現在のトータルスコア：' + score);
+            break;
+        case 'b':
+            console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
+            console.log('不正解です');
+            console.log('現在のトータルスコア：' + score);
+            break;
+        case 'c':
+            console.log('1問目の' + quiz_1.answer.value + 'を選択しました');
+            console.log('不正解です');
+            console.log('現在のトータルスコア：' + score);
+            break;
+    }
 }
+quiz_1.answer.value.addEventListener('click',answerQuiz1);
 
 function answerQuiz2(){
 //問題2の処理をここに記述
